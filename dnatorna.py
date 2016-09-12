070faddec6c28d2365a62349b430fae3bd986b46
Jingzhou Wang <jingzhou@caltech.edu>
2016-09-12 11:10:15 -0700

    Added DNA to RNA conversion module.

---
+++ dnatorna.py
@@ -0,0 +1,13 @@
+"""
+convert DNA sequences to RNA
+"""
+
+def rna(seq):
+    """
+    convert a DNA sequence to RNA.
+    """
+
+    # convert to uppercase
+    seq=seq.upper()
+
+    return seq.replace('T','U')
